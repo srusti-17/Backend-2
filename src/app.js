d592ff9a5db4e7d8d98854298b588a502e737c7e
Srusti Samikshya <srustisamikshya17@gmail.com>
2026-07-22 23:56:03 +0530

    integration in app

--- src/app.js
+++ src/app.js
@@ -21,6 +21,8 @@ import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //route declaration
 app.use("/api/v1/users",userRouter)
@@ -29,5 +31,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export {app}
